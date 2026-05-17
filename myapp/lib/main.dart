6e8c30860ac132c370ba1c17bb36b3e7bd7ca5e1
Vanshika kataria <vanshikakataria2005@gmail.com>
2026-05-17 19:51:57 +0530

    chap 2 completed

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MaterialApp(
     title: "awesome app", //when we minimise the app
@@ -17,9 +18,26 @@ class HomePage extends StatelessWidget { //here defines
         title: Text("awesome app"),  //actual title
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        child: Center(child: Text("hiooo flutter")),
-      ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10) ,
+              boxShadow: [BoxShadow(
+                color: Colors.grey,
+                blurRadius:5)
+                ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow,Colors.pink],
+              )
+            ),
+            child: Text("i am a box"),
+          ),
+        )
+     
     );
   }
 }
